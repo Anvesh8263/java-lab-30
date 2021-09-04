5b403ae89ddef8e78ff660012845f14effd23e35
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-04 09:26:10 +0530

    Add files via upload

---
+++ drawing.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;;
+class shape{
+    public String draw(){
+        return "Drawing shape";
+    }
+    public String erase(){
+        return "Erasing shape ";
+    }
+}
+public class drawing {
+    public static void main(String[] args) {
+        shape sp=new shape();
+        System.out.println(sp.draw());
+        System.out.println(sp.erase());
+    }
+}
