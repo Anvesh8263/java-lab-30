406f9112b195d74c1941a4ec5131a6816fd3c499
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-08 11:52:08 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,13 @@
+public class Shape {
+    public static void draw(){
+        System.out.println("Drawing Shape.");
+    }
+    public static void erase(){
+        System.out.println("Erasing Shape.");
+    }
+
+    public static void main(String[] args) {
+        draw();
+        erase();
+    }
+}
