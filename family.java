61564c5df775eac5800d45d8d94a38bafc21cf10
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-24 21:45:04 +0530

    Add files via upload

---
+++ family.java
@@ -0,0 +1,10 @@
+class family
+{
+        public static void main(String args[])
+        {
+                    System.out.println("YOGESH KUMAR KULSHRESTHA");
+                    System.out.println("RAKHI KULSHRESTHA");
+                    System.out.println("UNNATI KULSHRESTHA");
+                    System.out.println("ANVESH MANGALAM");
+        }
+}
