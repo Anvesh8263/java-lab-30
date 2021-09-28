6c1a3c5f8e1d09a68e531f761d7558c4dd6d6000
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-28 17:33:51 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,15 @@
+class demo
+{
+       void function()
+       {
+                   System.out.println("First Program");
+       }
+       public static void main(String args[])
+       {
+                 demo obj=new demo();
+                 
+       }
+}
+
+
+       
