712f4ae887fbb8928a8e7609219d0d11d4d6583b
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-30 16:58:51 +0530

    Add files via upload

---
+++ stringuppercase.java
@@ -0,0 +1,13 @@
+class stringuppercase
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="HeLLo We Are leaRnING JaVa";
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+
+                                          String res=str1.toUpperCase();
+                                          
+                                        System.out.println(res);
+                            }
+}
