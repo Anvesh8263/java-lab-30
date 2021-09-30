687054f1e9b0c9686d834ea1fa9072855eab4b14
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-30 16:22:01 +0530

    Add files via upload

---
+++ stringexample1.java
@@ -0,0 +1,14 @@
+class stringexample1
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="Java prog      ";
+
+                                          
+                                          System.out.println("\n\n-----------------\n\n");
+
+                                         String res=str1.trim();
+                                          
+                                        System.out.println(res);
+                            }
+}
