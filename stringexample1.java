9ed80952ca173f0d0345a31c17453f158a0853a6
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-30 16:35:03 +0530

    Add files via upload

--- stringexample1.java
+++ stringexample1.java
@@ -2,12 +2,11 @@ class stringexample1
 {
                          public static void main(String args[])
                          {
-                                          String str1="Java prog      ";
-
-                                          
+                                          String str1="HeLLo We Are leaRnING JaVa";
+                               
                                           System.out.println("\n\n-----------------\n\n");
 
-                                         String res=str1.trim();
+                                          String res=str1.replace("JaVa","Cloud");
                                           
                                         System.out.println(res);
                             }
