6bea5900bbe4c508c6fdd651e14d716b36e9e5d7
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-09-30 17:04:27 +0530

    Add files via upload

---
+++ stringlowercase.java
@@ -0,0 +1,13 @@
+class stringlowercase
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="HeLLo We Are leaRnING JaVa";
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+
+                                          String res=str1.toLowerCase();
+                                          
+                                        System.out.println(res);
+                            }
+}
