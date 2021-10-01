5a01b11ef445e0d2cde5d9883045ec2c5ce25dbc
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:15:30 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,13 @@
+class substring
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="JaVa Programpming";
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+                                           
+                                          String res=str1.substring(8);
+                                          
+                                        System.out.println(res);
+                            }
+}
