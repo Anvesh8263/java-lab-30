5582567094e2b31b8f49b48ca42f4a47bdafd883
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:15:08 +0530

    Add files via upload

---
+++ equals.java
@@ -0,0 +1,19 @@
+class equals
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="JaVa Programpming";
+
+                                         String str2="JaVa Programpming";
+                                         
+                                          System.out.println(str1);
+
+                                            System.out.println(str2);
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+                                           
+                                          boolean res=str1.equals(str2);
+                                          
+                                        System.out.println(res);
+                            }
+}
