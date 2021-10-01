45d694648d5db8f061ae910e1df37a007a889747
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:17:11 +0530

    Add files via upload

---
+++ charAt.java
@@ -0,0 +1,13 @@
+class charAt
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="JaVa Program";
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+                                           
+                                           char res=str1.charAt(5);
+                                          
+                                        System.out.println(res);
+                            }
+}
