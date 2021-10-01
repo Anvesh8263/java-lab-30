56d3d0cbd2f7ccbf6610db0979752a878cde551a
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:06:25 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,18 @@
+class reverse
+{
+                  public static void main(String args[])
+                 {
+                             StringBuffer sb1=new StringBuffer("Java");
+                             
+
+                             System.out.println(sb1);
+                             
+
+                                System.out.println("\n\n-------------------------------------------------------------------------\n\n");
+
+                               sb1.reverse();
+                              System.out.println(sb1);
+
+                         }
+}
+
