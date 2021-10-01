f338bc7172418372f5672d4307f45c9af4abc003
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:10:38 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,19 @@
+class concat
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="JaVa ";
+
+                                         String str2="Programpming";
+                                         
+                                          System.out.println(str1);
+
+                                            System.out.println(str2);
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+                                           
+                                          String res=str1.concat(str2);
+                                          
+                                        System.out.println(res);
+                            }
+}
