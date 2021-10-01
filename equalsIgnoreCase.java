9befea4039f43e89666242aee313189e0bb647a9
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:11:38 +0530

    Add files via upload

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,19 @@
+class equalsIgnoreCase
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="JaVa Programpming";
+
+                                         String str2="jaVa Programpming";
+                                         
+                                          System.out.println(str1);
+
+                                            System.out.println(str2);
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+                                           
+                                          boolean res=str1.equalsIgnoreCase(str2);
+                                          
+                                        System.out.println(res);
+                            }
+}
