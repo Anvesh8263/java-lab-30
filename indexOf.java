78024b13288e916cee903a42b619dabc2c418d7f
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-01 16:16:44 +0530

    Add files via upload

---
+++ indexOf.java
@@ -0,0 +1,13 @@
+class indexOf
+{
+                         public static void main(String args[])
+                         {
+                                          String str1="JaVa Programpming";
+                               
+                                          System.out.println("\n\n-----------------\n\n");
+                                           
+                                           int res=str1.indexOf('m');
+                                          
+                                        System.out.println(res);
+                            }
+}
