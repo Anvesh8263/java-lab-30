8d7deae9f5854d21c434718cf692b2523efac1c5
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-07 16:07:44 +0530

    Add files via upload

---
+++ stexample.java
@@ -0,0 +1,13 @@
+class stexample
+{
+     static void func()
+     {
+                  System.out.println("sample program");
+     }
+    public static void main(String args[])
+    {
+            stexample.func();
+            stexample obj = new stexample();
+            obj.func();
+    }
+}
