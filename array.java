a231e16c87dd77efc1a21140b8a1e2fcc9f86a7b
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-08 16:14:51 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+class array1
+{
+              public static void main(String args[])
+              {
+                             int ar[]=new int[5];
+
+
+                             ar[0]=15;
+                            ar[1]=25;
+                            ar[2]=35;
+                            ar[3]=45;
+                           ar[4]=55;
+                      
+                             System.out.println("----------------------------------Array Elements--------------------------------------");
+                              for(int i=0;i<ar.length;i++)
+                             {
+                                            System.out.println(ar[i]);
+                               }
+              }
+}
