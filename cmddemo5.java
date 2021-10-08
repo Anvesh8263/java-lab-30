9b36fb592885e0a10c589a2c2b6f4b599ae828e0
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-08 17:30:12 +0530

    Add files via upload

---
+++ cmddemo5.java
@@ -0,0 +1,13 @@
+class cmddemo5
+{
+           public static void main(String args[])
+           {
+                              String days[]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+
+                                  System.out.println("-------------------------------------Array Elements---------------------------------");
+                                 for(String var:days)
+                                 {
+                                                System.out.println(var);
+                                  }
+            } 
+}
