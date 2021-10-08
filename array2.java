257497dc787e5828cc4d1066acb3d0098de91d3f
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-08 16:16:52 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,15 @@
+class array2
+{
+              public static void main(String args[])
+              {
+                            String days[]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+
+                             
+                      
+                             System.out.println("----------------------------------Array Elements--------------------------------------");
+                             for(String var:days)
+                             {
+                                            System.out.println(var);
+                               }                    
+              }
+}
