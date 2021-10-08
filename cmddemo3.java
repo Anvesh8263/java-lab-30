59871df7af949d112e5cc53ae56370029dfdd0b6
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-08 17:29:33 +0530

    Add files via upload

---
+++ cmddemo3.java
@@ -0,0 +1,12 @@
+class cmddemo3
+{
+           public static void main(String args[])
+           {
+                                int size=args.length;
+                              System.out.println("No of Elements:"+size); 
+                               for(int i=0;i<size;i++) 
+                               {
+                                     System.out.println(args[i]); 
+                              }                      
+           }
+}
