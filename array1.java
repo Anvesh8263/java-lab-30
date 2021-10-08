57295e7fe33693e72c0c25cb859d24a7e057d1d5
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-08 16:15:36 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,16 @@
+class array1
+{
+              public static void main(String args[])
+              {
+                             int ar[]={10,20,30,40,50,60,70,80,50,90};
+
+                             
+                      
+                             System.out.println("----------------------------------Array Elements--------------------------------------");
+                              for(int i=0;i<ar.length;i++)
+                             {
+                                            System.out.println(ar[i]);
+                               }
+                                        System.out.println("No.of Elements:"+ar.length);
+              }
+}
