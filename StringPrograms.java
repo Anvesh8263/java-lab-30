f796e581cd4c036570d3c85f645dd2b5708805f3
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-10-21 21:49:13 +0530

    Add files via upload

---
+++ StringPrograms.java
@@ -0,0 +1,24 @@
+// How to reverse a String in Java?
+
+public class StringPrograms {
+
+	public static void main(String[] args) {
+
+		String str = "123";
+		System.out.println(reverse(str));
+	}
+
+	public static String reverse(String in) {
+		if (in == null)
+			throw new IllegalArgumentException("Null is not valid input");
+
+		StringBuilder out = new StringBuilder();
+
+		char[] chars = in.toCharArray();
+
+		for (int i = chars.length - 1; i >= 0; i--)
+			out.append(chars[i]);
+
+		return out.toString();
+	}
+}
