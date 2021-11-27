d9777f391bb0724b9ca06f90299605012515cfb3
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-11-27 20:53:02 +0530

    Add files via upload

---
+++ setdemo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class setdemo
+{
+       void func()
+       {
+                              Set st=new HashSet();
+           
+                              st.add("Coke");
+                               st.add("Perk");
+                                 st.add("2000");
+                                st.add("Zehar");
+
+                              System.out.println(st);
+                              System.out.println("No. of items:"+st.size());
+       }
+       public static void main(String args[])
+       {
+                     setdemo obj=new setdemo();
+                     obj func();
+       }
+}
+                     
+                 
+
