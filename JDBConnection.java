94f177f32e50cffca64383e77b50a69c677e4a8f
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2021-11-30 21:24:28 +0530

    Add files via upload

---
+++ JDBConnection.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+public class JDBConnection 
+{
+    JDBConnection()
+    { 
+        try
+        {
+            System.out.println("");
+        }
+        catch(Exception e)
+        {
+            System.out.println("");
+        }    
+    }
+    public static void main(String args[])
+    {
+     JDBConnection obj=new JDBConnection();   
+    }
+}
